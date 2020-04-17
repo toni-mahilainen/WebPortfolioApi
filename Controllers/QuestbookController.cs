45bf4c43d7125d1a76257d9543c7af697a5a46e0
Toni Mahilainen <45209240+point-toni-mahilainen@users.noreply.github.com>
2020-04-17 17:30:05 +0300

    Ready questbook controller
    * GetAllMessages()
    
    * AddNewMessage()
    
    * DeleteMessages()

--- Controllers/QuestbookController.cs
+++ Controllers/QuestbookController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebPortfolioCoreApi.Models;
 
 namespace WebPortfolioCoreApi.Controllers
 {
@@ -11,5 +12,134 @@ namespace WebPortfolioCoreApi.Controllers
     [ApiController]
     public class QuestbookController : ControllerBase
     {
+        // GET: api/questbook/
+        // Get all visitors + messages
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult GetAllMessages(int id)
+        {
+            WebPortfolioContext context = new WebPortfolioContext();
+
+            try
+            {
+                // Searching right portfolio with user ID
+                var portfolioId = (from p in context.PortfolioContent
+                                 where p.UserId == id
+                                 select p.PortfolioId).FirstOrDefault();
+
+                // Searching all visitors/messages with portfolio ID
+                var questbookContent = (from q in context.QuestbookMessages
+                                        where q.PortfolioId == portfolioId
+                                        join v in context.Visitors
+                                        on q.VisitorId equals v.VisitorId
+                                        select new 
+                                        { 
+                                            v.Firstname,
+                                            v.Lastname,
+                                            v.Company,
+                                            q.VisitationTimestamp,
+                                            q.Message
+                                        }).ToList();
+                               
+                return Ok(questbookContent);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Problem detected while searching messages for questbook. Error message: " + ex.Message);
+            }
+            finally
+            {
+                context.Dispose();
+            }
+        }
+
+        // POST: api/questbook/
+        // Add new message
+        [HttpPost]
+        [Route("{id}")]
+        public ActionResult AddNewMessage(int id, [FromBody] NewMessage newMessage)
+        {
+            WebPortfolioContext context = new WebPortfolioContext();
+
+            try
+            {
+                // Placed visitor info to an object and adding it to database
+                Visitors visitor = new Visitors
+                {
+                    Firstname = newMessage.VisitorFirstname,
+                    Lastname = newMessage.VisitorLastname,
+                    Company = newMessage.VisitorCompany
+                };
+
+                context.Visitors.Add(visitor);
+                context.SaveChanges();
+
+                // Searching last added visitor ID and adding other message and other information to database
+                int visitorId = (from v in context.Visitors
+                                 orderby v.VisitorId ascending
+                                 select v.VisitorId).LastOrDefault();
+
+                QuestbookMessages messageAndOthers = new QuestbookMessages
+                {
+                    PortfolioId = id,
+                    VisitorId = visitorId,
+                    Message = newMessage.Message,
+                    VisitationTimestamp = newMessage.VisitationTimestamp
+                };
+                
+                context.QuestbookMessages.Add(messageAndOthers);
+                context.SaveChanges();
+
+                return Ok("New message has saved!");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Problem detected while adding a new message. Error message: " + ex.Message);
+            }
+            finally
+            {
+                context.Dispose();
+            }
+        }
+
+        // DELETE: api/questbook/{id}
+        // Delete message
+        [HttpDelete]
+        [Route("{id}")]
+        public ActionResult DeleteMessage(int id)
+        {
+            WebPortfolioContext context = new WebPortfolioContext();
+
+            try
+            {
+                // Searching right message with ID
+                var message = context.QuestbookMessages.Find(id);
+
+                // Searching a visitor who wrote the message
+                int visitorId = (from v in context.Visitors
+                                 where v.VisitorId == message.VisitorId
+                                 select v.VisitorId).FirstOrDefault();
+
+                var visitor = context.Visitors.Find(visitorId);
+
+                // Deletion from the database is performed
+                if (message != null && visitor != null)
+                {
+                    context.Remove(message);
+                    context.Remove(visitor);
+                    context.SaveChanges();
+                }
+
+                return Ok("Message deleted succesfully!");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Problem detected while deleting user. Error message: " + ex.Message);
+            }
+            finally
+            {
+                context.Dispose();
+            }
+        }
     }
 }
---
+++ Models/NewMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebPortfolioCoreApi.Models
+{
+    public class NewMessage
+    {
+        public string VisitorFirstname { get; set; }
+
+        public string VisitorLastname { get; set; }
+
+        public string VisitorCompany { get; set; }
+
+        public string Message { get; set; }
+
+        public DateTime VisitationTimestamp { get; set; }
+    }
+}
