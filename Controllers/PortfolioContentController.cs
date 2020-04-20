289a6b1f5554c109542444c7b3add8ffa2c1a53b
Toni Mahilainen <toni.mahilainen@student.careeria.fi>
2020-04-20 10:32:16 +0300

    GetAllContent(), GetAllEmails(), GetAllImages()

---
+++ Controllers/PortfolioContentController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebPortfolioCoreApi.Models;
+
+namespace WebPortfolioCoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PortfolioContentController : ControllerBase
+    {
+        // GET: api/portfoliocontent/content/{portfolioId}
+        // Get all portfolio content
+        [HttpGet]
+        [Route("content/{id}")]
+        public ActionResult GetAllContent(int id)
+        {
+            WebPortfolioContext context = new WebPortfolioContext();
+
+            try
+            {
+                var content = (from pc in context.PortfolioContent
+                               where pc.UserId == id
+                               select new 
+                               {
+                                   pc.Firstname,
+                                   pc.Lastname,
+                                   pc.Birthdate,
+                                   pc.City,
+                                   pc.Country,
+                                   pc.Phonenumber,
+                                   pc.Punchline,
+                                   pc.BasicKnowledge,
+                                   pc.Education,
+                                   pc.WorkHistory,
+                                   pc.LanguageSkills,
+                               }).ToList();
+
+                return Ok(content);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Problem detected while getting portfolio content for user " + id + ". Error message: " + ex.Message);
+            }
+            finally
+            {
+                context.Dispose();
+            }
+        }
+
+        // GET: api/portfoliocontent/emails/{portfolioId}
+        // Get all users email addresses
+        [HttpGet]
+        [Route("emails/{id}")]
+        public ActionResult GetAllEmails(int id)
+        {
+            WebPortfolioContext context = new WebPortfolioContext();
+
+            try
+            {
+                var emails = (from e in context.Emails
+                               where e.PortfolioId == id
+                               select e.EmailAddress).ToList();
+
+                return Ok(emails);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Problem detected while getting email addresses for user " + id + ". Error message: " + ex.Message);
+            }
+            finally
+            {
+                context.Dispose();
+            }
+        }
+
+        // GET: api/portfoliocontent/images/{portfolioId}
+        // Get all users images
+        [HttpGet]
+        [Route("images/{id}")]
+        public ActionResult GetAllImages(int id)
+        {
+            WebPortfolioContext context = new WebPortfolioContext();
+
+            try
+            {
+                var images = (from i in context.ImageUrls
+                              where i.PortfolioId == id
+                              select new 
+                              { 
+                                  i.TypeId, 
+                                  i.Url 
+                              }).ToList();
+
+                return Ok(images);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Problem detected while getting images for user " + id + ". Error message: " + ex.Message);
+            }
+            finally
+            {
+                context.Dispose();
+            }
+        }
+    }
+}
