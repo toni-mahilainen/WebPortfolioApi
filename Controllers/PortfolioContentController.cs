466dd206d213e11013dd78bf3ef02649e9200397
Toni Mahilainen <toni.mahilainen@student.careeria.fi>
2020-04-23 15:33:24 +0300

    SkillsController ja ImagesController lisätty

--- Controllers/PortfolioContentController.cs
+++ Controllers/PortfolioContentController.cs
@@ -21,7 +21,7 @@ namespace WebPortfolioCoreApi.Controllers
         // Get all portfolio content
         [HttpGet]
         [Route("content/{id}")]
-        public ActionResult GetAllContent(int id)
+        public ActionResult GetContent(int id)
         {
             WebPortfolioContext context = new WebPortfolioContext();
 
@@ -60,7 +60,7 @@ namespace WebPortfolioCoreApi.Controllers
         // Get all users email addresses
         [HttpGet]
         [Route("emails/{id}")]
-        public ActionResult GetAllEmails(int id)
+        public ActionResult GetEmails(int id)
         {
             WebPortfolioContext context = new WebPortfolioContext();
 
@@ -75,36 +75,6 @@ namespace WebPortfolioCoreApi.Controllers
             catch (Exception ex)
             {
                 return BadRequest("Problem detected while getting email addresses for user " + id + ". Error message: " + ex.Message);
-            }
-            finally
-            {
-                context.Dispose();
-            }
-        }
-
-        // GET: api/portfoliocontent/images/{userId}
-        // Get all users images
-        [HttpGet]
-        [Route("images/{id}")]
-        public ActionResult GetAllImages(int id)
-        {
-            WebPortfolioContext context = new WebPortfolioContext();
-
-            try
-            {
-                var images = (from i in context.ImageUrls
-                              where i.UserId == id
-                              select new
-                              {
-                                  i.TypeId,
-                                  i.Url
-                              }).ToList();
-
-                return Ok(images);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest("Problem detected while getting images for user " + id + ". Error message: " + ex.Message);
             }
             finally
             {
@@ -167,102 +137,6 @@ namespace WebPortfolioCoreApi.Controllers
             catch (Exception ex)
             {
                 return BadRequest("Problem detected while adding portfolio content for user " + newContent.Firstname + " " + newContent.Lastname + ". Error message: " + ex.Message);
-            }
-            finally
-            {
-                context.Dispose();
-            }
-        }
-
-        // POST: api/portfoliocontent/images/{userId}
-        // Add users images to database
-        [HttpPost]
-        [Route("images/{id}")]
-        public ActionResult AddImages(int id, [FromBody] ImageUrls newImageInfo)
-        {
-            WebPortfolioContext context = new WebPortfolioContext();
-
-            try
-            {
-                // Adds image urls to database
-                ImageUrls newImage = new ImageUrls
-                {
-                    UserId = id,
-                    TypeId = newImageInfo.TypeId,
-                    Url = newImageInfo.Url
-                };
-
-                context.ImageUrls.Add(newImage);
-                context.SaveChanges();
-
-                return Ok("New image has saved!");
-            }
-            catch (Exception ex)
-            {
-                return BadRequest("Problem detected while adding image for user " + id + ". Error message: " + ex.Message);
-            }
-            finally
-            {
-                context.Dispose();
-            }
-        }
-
-        // POST: api/portfoliocontent/skill/{userId}
-        // Add users skills to database
-        [HttpPost]
-        [Route("skill/{id}")]
-        public ActionResult AddSkill(int id, [FromBody] JsonElement jsonElement)
-        {
-            WebPortfolioContext context = new WebPortfolioContext();
-
-            // Converts json element to string
-            string json = System.Text.Json.JsonSerializer.Serialize(jsonElement);
-
-            // Converts json string to JSON object
-            var obj = JsonConvert.DeserializeObject<JObject>(json);
-
-            try
-            {
-                // Adds new skill to database
-                Skills skill = new Skills
-                {
-                    UserId = id,
-                    Skill = obj["Skill"]["SkillName"].ToString(),
-                    SkillLevel = int.Parse(obj["Skill"]["SkillLevel"].ToString())
-                };
-
-                context.Skills.Add(skill);
-                context.SaveChanges();
-
-                // Get last added skill ID for specific user
-                int skillId = (from s in context.Skills
-                               where s.UserId == id
-                               orderby s.SkillId ascending
-                               select s.SkillId).Last();
-
-                // Converts nested "Projects" JSON object to array and save count of an array to variable
-                var projectsArray = obj["Projects"].ToArray();
-                int arrayCount = projectsArray.Count();
-
-                // Adds as many projects as the count of an array indicates
-                for (int i = 0; i < arrayCount; i++)
-                {
-                    Projects project = new Projects
-                    {
-                        SkillId = skillId,
-                        Name = obj["Projects"][i]["Name"].ToString(),
-                        Link = obj["Projects"][i]["Link"].ToString(),
-                        Description = obj["Projects"][i]["Description"].ToString()
-                    };
-                    context.Projects.Add(project);
-                    context.SaveChanges();
-                }
-
-                return Ok("New skill has saved!");
-            }
-            catch (Exception ex)
-            {
-                return BadRequest("Problem detected while adding image for user " + id + ". Error message: " + ex.Message);
             }
             finally
             {
@@ -346,62 +220,6 @@ namespace WebPortfolioCoreApi.Controllers
             catch (Exception ex)
             {
                 return BadRequest("Problem detected while updating email address. Error message: " + ex.Message);
-            }
-            finally
-            {
-                context.Dispose();
-            }
-        }
-
-        // PUT: api/portfoliocontent/images/{userId}
-        // Update users images
-        [HttpPut]
-        [Route("images/{id}")]
-        public ActionResult UpdateImages(int id, [FromBody] JsonElement jsonElement)
-        {
-            WebPortfolioContext context = new WebPortfolioContext();
-
-            // Converts json element to string
-            string json = System.Text.Json.JsonSerializer.Serialize(jsonElement);
-
-            // Converts json string to dataset
-            DataSet dataSet = JsonConvert.DeserializeObject<DataSet>(json);
-
-            try
-            {
-                if (dataSet != null)
-                {
-                    // Get through every table from dataset
-                    // Tables includes type ID and new URL for image
-                    foreach (DataTable dataTable in dataSet.Tables)
-                    {
-                        DataRow row = dataTable.Rows[0];
-
-                        int typeId = int.Parse(row["TypeID"].ToString());
-                        string newUrl = row["Url"].ToString();
-
-                        ImageUrls oldImage = (from iu in context.ImageUrls
-                                              where iu.UserId == id && iu.TypeId == typeId
-                                              select iu).FirstOrDefault();
-
-                        // If image url has changed, it will be updated to database
-                        if (newUrl != oldImage.Url)
-                        {
-                            oldImage.Url = newUrl;
-                            context.SaveChanges();
-                        }
-                    }
-
-                    return Ok("Images updated succesfully!");
-                }
-                else
-                {
-                    return NotFound("Content not found.");
-                }
-            }
-            catch (Exception ex)
-            {
-                return BadRequest("Problem detected while updating images. Error message: " + ex.Message);
             }
             finally
             {
