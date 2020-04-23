f3b7e36b55b2102be31067320660c333cbfd77f2
Toni Mahilainen <toni.mahilainen@student.careeria.fi>
2020-04-23 19:12:08 +0300

    GetProjects()

---
+++ Controllers/ProjectsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebPortfolioCoreApi.Models;
+
+namespace WebPortfolioCoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        // GET: api/projects/{skillId}
+        // Get all users projects for specific skill
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult GetProjects(int id)
+        {
+            WebPortfolioContext context = new WebPortfolioContext();
+
+            try
+            {
+                var projects = (from p in context.Projects
+                              where p.SkillId == id
+                              select p).ToList();
+
+                return Ok(projects);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Problem detected while getting skills for user " + id + ". Error message: " + ex.Message);
+            }
+            finally
+            {
+                context.Dispose();
+            }
+        }
+    }
+}
