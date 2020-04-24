28cf4916d6a370f3fb3f711cc1f16dac9cf509ae
Toni Mahilainen <toni.mahilainen@student.careeria.fi>
2020-04-24 10:04:34 +0300

    AddLink()

--- Controllers/SocialMediaController.cs
+++ Controllers/SocialMediaController.cs
@@ -16,7 +16,7 @@ namespace WebPortfolioCoreApi.Controllers
         // Get all social media links for users portfolio
         [HttpGet]
         [Route("{id}")]
-        public ActionResult GetSocialMediaLinks(int id)
+        public ActionResult GetLinks(int id)
         {
             WebPortfolioContext context = new WebPortfolioContext();
 
@@ -37,6 +37,51 @@ namespace WebPortfolioCoreApi.Controllers
             catch (Exception ex)
             {
                 return BadRequest("Problem detected while getting links for portfolio " + id + ". Error message: " + ex.Message);
+            }
+            finally
+            {
+                context.Dispose();
+            }
+        }
+
+        // POST: api/socialmedia/{portfolioId}
+        // Add new social media link for specific portfolio
+        [HttpPost]
+        [Route("{id}")]
+        public ActionResult AddLink(int id, [FromBody] SocialMediaLinks newLink)
+        {
+            WebPortfolioContext context = new WebPortfolioContext();
+
+            try
+            {
+                // Check if user already have a link for service. If not an addition is made to the database
+                SocialMediaLinks linkCheck = (from sml in context.SocialMediaLinks
+                                              where sml.ServiceId == newLink.ServiceId
+                                              select sml).FirstOrDefault();
+
+                if (linkCheck == null)
+                {
+                    // Placed new link info to an object and adding it to database
+                    SocialMediaLinks link = new SocialMediaLinks
+                    {
+                        PortfolioId = id,
+                        ServiceId = newLink.ServiceId,
+                        Link = newLink.Link
+                    };
+
+                    context.SocialMediaLinks.Add(link);
+                    context.SaveChanges();
+                }
+                else
+                {
+                    return NotFound("Portfolio already includes a link for service.");
+                }
+
+                return Ok("New link has added to portfolio!");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Problem detected while adding a new link. Error message: " + ex.Message);
             }
             finally
             {
